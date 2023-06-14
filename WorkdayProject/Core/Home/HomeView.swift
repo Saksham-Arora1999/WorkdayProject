6801dd9557c45943ed8c3a29dff9d638d0206016
Saksham <instant.saksham@gmail.com>
2023-06-13 22:53:01 -0700

    Initial file structure

---
+++ WorkdayProject/Core/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  WorkdayProject
+//
+//  Created by user242726 on 6/13/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
