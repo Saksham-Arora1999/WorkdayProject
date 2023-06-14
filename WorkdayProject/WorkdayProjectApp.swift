d7dd7c50fca5abe8eec2d2cf999f84e78d85b14e
Saksham <instant.saksham@gmail.com>
2023-06-13 22:34:35 -0700

    Initial Commit

---
+++ WorkdayProject/WorkdayProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkdayProjectApp.swift
+//  WorkdayProject
+//
+//  Created by user242726 on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkdayProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
